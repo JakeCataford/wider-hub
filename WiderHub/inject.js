60c24a4e23067e5da155b443a02f1c786111d4b4
Jake Cataford <jakecataford@gmail.com>
2013-06-14 15:13:57 -0400

    Updates Script

---
+++ WiderHub/inject.js
@@ -0,0 +1,11 @@
+if($(".view-pull-request")[0]) {
+	$('#js-repo-pjax-container').css({
+		width : '100%',
+    	margin : '10px'
+	});
+
+	$('.discussion-timeline').css({
+		width : window.innerWidth - 110
+	});
+}
+
