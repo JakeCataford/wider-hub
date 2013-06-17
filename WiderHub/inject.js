8aadfafe0e975b5d3fea349bb36771acbd4b0a4d
Jake Cataford <jakecataford@gmail.com>
2013-06-17 15:56:56 -0400

    Updated For New Gihub Repos

--- WiderHub/inject.js
+++ WiderHub/inject.js
@@ -1,3 +1,15 @@
+
+if($(".repository-sidebar")[0]) {
+
+	$('.repo-label').hide();
+	$('.container').width(window.innerWidth - 40);
+	$('.container').css({margin : "0px 0px 0px 25px"});
+	$('#js-repo-pjax-container').width(window.innerWidth - 100);
+	$('.discussion-timeline').width(window.innerWidth - 215);
+}
+
+
+/*
 if($(".view-pull-request")[0]) {
 	$('#js-repo-pjax-container').css({
 		width : '100%',
@@ -8,4 +20,4 @@ if($(".view-pull-request")[0]) {
 		width : window.innerWidth - 110
 	});
 }
-
+*/
