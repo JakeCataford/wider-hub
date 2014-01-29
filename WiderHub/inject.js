bea7a1bfcdd9b2924e78e12b06d4e6f6033cc4b2
JakeCataford <JakeCataford@Gmail.com>
2014-01-29 14:55:50 -0500

    Update extention for new github layout.

--- WiderHub/inject.js
+++ WiderHub/inject.js
@@ -1,23 +1,16 @@
-
-if($(".view-pull-request")[0]) {
-
-	$('.repo-label').hide();
-	$('.container').width(window.innerWidth - 40);
-	$('.container').css({margin : "0px 0px 0px 25px"});
-	$('#js-repo-pjax-container').width(window.innerWidth - 100);
-	$('.discussion-timeline').width(window.innerWidth - 215);
+var widerhub = {
+	init : function() {	
+		$('.repo-label').hide();
+		$('.container').width(window.innerWidth - 40);
+		$('.container').css({margin : "0px 0px 0px 25px"});
+		$('#js-repo-pjax-container').width(window.innerWidth - 100);
+		$('.discussion-timeline').width(window.innerWidth - $('.discussion-sidebar').width() - 215);
+	}
 }
 
 
-/*
 if($(".view-pull-request")[0]) {
-	$('#js-repo-pjax-container').css({
-		width : '100%',
-    	margin : '10px'
-	});
-
-	$('.discussion-timeline').css({
-		width : window.innerWidth - 110
-	});
+	$(window).resize(widerhub.init);
+	widerhub.init();
+	
 }
-*/
